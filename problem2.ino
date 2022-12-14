d5563cbf0ff7e831102c44061f5105cf3ef79310
OliverCreations <113934136+OliverCreations@users.noreply.github.com>
2022-12-14 11:39:59 -0700

    Create problem2.ino

---
+++ problem2.ino
@@ -0,0 +1,18 @@
+/*
+
+*/
+int red=11;
+int green=10;
+int blue=9;
+void setup() {
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+  pinMode(blue,OUTPUT);
+  analogWrite(red,128);
+  analogWrite(green,2);
+  analogWrite(blue,16);
+}
+
+void loop() {
+    
+}
